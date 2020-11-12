6b43cf4286dd6b480d7ab3c30ac8ef8756695f76
Ankit Gupta <guptaankit2408@gmail.com>
2020-11-13 01:17:43 +0530

    heroku deploy

--- textutils/settings.py
+++ textutils/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,3 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+django_heroku.settings(locals())
